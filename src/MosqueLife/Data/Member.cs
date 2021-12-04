b3bf025e306cf314ebf679115c918ba38426a135
Selmirrrrr <git@hajruli.ch>
2021-12-05 00:05:37 +0100

    feat; members view wip

---
+++ src/MosqueLife/Data/Member.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosqueLife.Data
+{
+    public class Member
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+
+    }
+
+    public class Address
+    {
+        public Guid Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(200)]
+        [Required]
+        public string StreetLine1 { get; set; }
+
+        [MaxLength(200)]
+        public string? StreetLine2 { get; set; }
+
+        [Required]
+        public string HouseNumber { get; set; }
+
+        [Range(1000, 999999)]
+        public int ZipCode { get; set; }
+
+        [MinLength(3)]
+        [Required]
+        public string City { get; set; }
+
+
+        public string State { get; set; }
+        public string Country { get; set; }
+
+    }
+}
