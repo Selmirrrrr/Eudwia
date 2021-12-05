5112130378d9fec831525ea93e5b16b2391a8dd7
Selmirrrrr <git@hajruli.ch>
2021-12-05 23:34:52 +0100

    chore: trololol

--- src/MosqueLife/Program.cs
+++ src/MosqueLife/Program.cs
@@ -26,14 +26,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    Console.WriteLine("DEV");
     app.UseMigrationsEndPoint();
 }
 else
 {
+    Console.WriteLine("NO_DEV");
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+#if DEBUG
+Console.WriteLine("DEBUG");
+#else
+Console.WriteLine("NO_DEBUG");
+#endif
+
 
 app.UseHttpsRedirection();
 
