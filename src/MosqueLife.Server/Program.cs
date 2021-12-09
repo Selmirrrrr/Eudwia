757fbb842cfbbff6a25f029b77ac7e68f334bcd6
Selmirrrrr <git@hajruli.ch>
2021-12-09 02:08:38 +0100

    feat: ef core db migrations

--- src/MosqueLife.Server/Program.cs
+++ src/MosqueLife.Server/Program.cs
@@ -1,10 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MosqueLife.Server.Data.Contexts;
+
 namespace MosqueLife.Server;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+
+        using var scope = host.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
