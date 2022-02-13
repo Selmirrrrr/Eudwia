d9d4efe0919a1e99fb5c83967a8a22ac2a30d8fd
Selmirrrrr <git@hajruli.ch>
2022-02-13 19:45:11 +0100

    feat: possibility to set a new password for connected user

--- src/Eudwia.Server/Features/Members/Security/MembersSecuritySetPasswordEndpoint.cs
+++ src/Eudwia.Server/Features/Members/Security/MembersSecuritySetPasswordEndpoint.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Eudwia.Server.Data;
 using Eudwia.Shared.Authorization;
+using Eudwia.Shared.Extensions;
 using Eudwia.Shared.Features.Members.Security;
 using Microsoft.AspNetCore.Identity;
 
@@ -15,10 +16,12 @@ namespace Eudwia.Server.Features.Members.Security;
 public class MembersSecuritySetPasswordEndpoint : ControllerBase
 {
     private readonly UserManager<Member> _userManager;
+    private readonly IAuthorizationService _authorizationService;
 
-    public MembersSecuritySetPasswordEndpoint(UserManager<Member> userManager)
+    public MembersSecuritySetPasswordEndpoint(UserManager<Member> userManager, IAuthorizationService authorizationService)
     {
         _userManager = userManager;
+        _authorizationService = authorizationService;
     }
 
     [HttpPost("members/{memberId:guid}/password")]
@@ -27,8 +30,14 @@ public class MembersSecuritySetPasswordEndpoint : ControllerBase
     public async Task<ActionResult> Handle([FromRoute] Guid memberId, [FromBody] MembersSecuritySetPasswordCommand command)
     {
         var member = await _userManager.FindByIdAsync(memberId.ToString());
-        
         if (member is null) return NotFound(memberId);
+
+        if (!User.IsInRoleAdmin() && !User.IsInRoleSuperAdmin())
+        {
+            var authorizationResult = await _authorizationService.AuthorizeAsync(User, memberId, Policies.IsCurrentdUser);
+            if (!authorizationResult.Succeeded)
+                return Forbid();
+        }
 
         var token = await _userManager.GeneratePasswordResetTokenAsync(member);
         var result = await _userManager.ResetPasswordAsync(member, token, command.Password);
