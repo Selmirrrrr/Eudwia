dc3fcb79c0b9fb81d23508cbdd225ce5bf66dba5
Selmirrrrr <git@hajruli.ch>
2022-02-13 19:28:07 +0100

    feat: possibility to set a new password for a member

--- src/Eudwia.Shared/Features/Members/Security/MembersSecuritySetPasswordCommand.cs
+++ src/Eudwia.Shared/Features/Members/Security/MembersSecuritySetPasswordCommand.cs
@@ -2,26 +2,26 @@
 
 namespace Eudwia.Shared.Features.Members.Security;
 
-public record MembersPasswordUpdateCommand
+public record MembersSecuritySetPasswordCommand
 {
     public string Password { get; set; } = string.Empty;
 
     public string ConfirmPassword { get; set; } = string.Empty;
 }
 
-public class MembersPasswordUpdateCommandValidator : AbstractValidator<MembersPasswordUpdateCommand>
+public class MembersSecuritySetPasswordCommandValidator : AbstractValidator<MembersSecuritySetPasswordCommand>
 {
-    public MembersPasswordUpdateCommandValidator()
+    public MembersSecuritySetPasswordCommandValidator()
     {
         RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
-        RuleFor(x => x.Password)
-            .Equal(x => x.ConfirmPassword)
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.Password)
             .When(x => !string.IsNullOrWhiteSpace(x.Password));
     }
 
     public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
     {
-        var result = await ValidateAsync(ValidationContext<MembersPasswordUpdateCommand>.CreateWithOptions((MembersPasswordUpdateCommand) model, x => x.IncludeProperties(propertyName)));
+        var result = await ValidateAsync(ValidationContext<MembersSecuritySetPasswordCommand>.CreateWithOptions((MembersSecuritySetPasswordCommand) model, x => x.IncludeProperties(propertyName)));
         return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
     };
 }
